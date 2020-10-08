9b2022d625b688b84c4e1580b42f8eae7c0f024a
S_Motlaq <motlaq@aut.ac.ir>
2020-10-08 15:14:21 +0330

    first commit

---
+++ my_watch_software/MDK-ARM/RTE/_my_watch_software/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'my_watch_software' 
+ * Target:  'my_watch_software' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
